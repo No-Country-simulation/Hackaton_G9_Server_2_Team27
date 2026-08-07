e1522adf6ad43beb990f18d62b3a385e66284665
Yadira Shasenka Gamboa Quispe <yadiragamboaq.05@gmail.com>
2026-08-06 19:54:07 -0500

    feat(backend): agregar anotación @CrossOrigin en AnalisisEnergeticoController para permitir conexión con frontend

--- backend/Backend/src/main/java/EnegiAI/Backend/controller/AnalisisEnergeticoController.java
+++ backend/Backend/src/main/java/EnegiAI/Backend/controller/AnalisisEnergeticoController.java
@@ -22,6 +22,7 @@ import java.util.List;
  * para posteriormente devolver una respuesta HTTP apropiada.
  */
 @RestController
+@CrossOrigin(origins = "*")
 public class AnalisisEnergeticoController {
 
 
