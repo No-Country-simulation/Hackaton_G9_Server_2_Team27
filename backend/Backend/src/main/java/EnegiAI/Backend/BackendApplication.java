d5235fde523934bc83f21ca27217380e65a83d7b
thxlopez7 <thiago.2003.lopez@gmail.com>
2026-08-20 21:55:16 -0300

    feat: Solucion de errores V4

--- backend/Backend/src/main/java/EnegiAI/Backend/BackendApplication.java
+++ backend/Backend/src/main/java/EnegiAI/Backend/BackendApplication.java
@@ -2,9 +2,6 @@ package EnegiAI.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -13,17 +10,5 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("*")
-						.allowedHeaders("*");
-			}
-		};
-	}
 
 }
